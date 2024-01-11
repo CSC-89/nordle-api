9e20609b83b15d9aa0ffe9f7a76e6613d8ab5de4
Chris Stephens <chris.stephens@appliedtechnology.se>
2024-01-11 16:23:56 +0100

    remove: unused throw

--- Wordle.Api/Services/DictionaryApiService.cs
+++ Wordle.Api/Services/DictionaryApiService.cs
@@ -27,7 +27,5 @@ public class DictionaryApiService : IDictionaryApiService
         // }).catch((error) => {
         //     console.error(error);
         // });
-        
-        throw new NotImplementedException();
     }
 }
