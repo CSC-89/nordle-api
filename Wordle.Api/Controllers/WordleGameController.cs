85eaf7f94746a015dca505131420a80c1eb99c93
Chris Stephens <chris.stephens@appliedtechnology.se>
2024-01-11 16:03:48 +0100

    fix: First test

--- Wordle.Api/Controllers/WordleGameController.cs
+++ Wordle.Api/Controllers/WordleGameController.cs
@@ -6,27 +6,9 @@ namespace Wordle.Api.Controllers;
 [Route("[controller]")]
 public class WordleGameController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
+    [HttpGet(Name = "GetWordleAnswer")]
+    public string GetWordleAnswer()
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    private readonly ILogger<WordleGameController> _logger;
-
-    public WordleGameController(ILogger<WordleGameController> logger)
-    {
-        _logger = logger;
-    }
-
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return "LOVEN";
     }
 }
